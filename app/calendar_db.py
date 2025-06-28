1ed5c0b104a79ee3bf472b591949d80c3efec7da
anony <anonymoususer5761@gmail.com>
2025-06-28 13:38:51 +0530

    Added the ability to choose date and improved choosing time UI by using native HTML tools like type="time".

--- app/calendar_db.py
+++ app/calendar_db.py
@@ -1,7 +1,5 @@
 from app.database_manager import get_db
 
-from collections import namedtuple
-
 def get_years():
     db = get_db()
     years = db.execute("SELECT DISTINCT year FROM calendar").fetchall()
@@ -17,18 +15,29 @@ def get_months():
 
 def get_dates(month, year):
     db = get_db()
-    dates = db.execute("SELECT day_id, date FROM calendar WHERE year = ? AND month_id = ?", (year, month,)).fetchall()
+    dates = db.execute("SELECT id, day_id, date FROM calendar WHERE year = ? AND month_id = ?", (year, month,)).fetchall()
     db.close()
 
-    Date = namedtuple("date", ["day_id", "date"])
-    my_dates = []
-    for date in dates:
-        my_dates.append(Date(date["day_id"], date["date"])._asdict())
-    return my_dates
+    dict_dates = [{"id": date["id"], "day_id": date["day_id"], "date": date["date"]} for date in dates]
+    return dict_dates
 
 
-def get_day_name(year: str, month: str, day: str) -> str:
+def get_day_name(date_id: int) -> str:
     db = get_db()
-    name: str = db.execute("SELECT day FROM days JOIN calendar ON days.id = calendar.day_id WHERE year = ? AND month_id = ? AND date = ?", (year, month, day,)).fetchone()[0]
+    cell = db.execute("SELECT day FROM days JOIN calendar ON days.id = calendar.day_id WHERE calendar.id = ?", (date_id,)).fetchone()
+    if cell == None:
+        name = "fake-name"
+    else:
+        name = cell["day"]
     db.close()
     return name
+
+
+def get_date(date_id):
+    db = get_db()
+    cell = db.execute("SELECT printf('%04d-%02d-%02d', year, month_id, date) as formatted_date FROM calendar WHERE calendar.id = ?", (date_id,)).fetchone()
+    if cell == None:
+        date = "fake-date"
+    else:
+        date = cell["formatted_date"]
+    return date
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 from app import app
-from app.calendar_db import get_dates, get_months, get_years, get_day_name
+from app.calendar_db import get_dates, get_months, get_years, get_day_name, get_date
 from app.error_handler import error
 from app.forms import LoginForm, RegistrationForm
 from app.user import sign_in_user, register_user
+from app.helpers import verify_request_source
 
 from flask import render_template, request, redirect, url_for, flash, jsonify
 from flask_login import logout_user, login_required, current_user
+
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -18,9 +21,10 @@ def index():
 
 @app.route("/dates", methods=["GET", "POST"])
 def dates():
-    year, month, day = request.args.get("year"), request.args.get("month"), request.args.get("day")
-    day_name = get_day_name(year, month, day)
-    return render_template("dates.html", year=year, month=month, day=day, day_name=day_name)
+    date_id = request.args.get("date-id")
+    day_name = get_day_name(date_id)
+    date = get_date(date_id)
+    return render_template("dates.html", date=date, day_name=day_name)
 
 
 @app.route("/settings")
@@ -84,6 +88,20 @@ def api_test():
     return jsonify(return_value)
 
 # The below routes are APIs
+@app.route("/api/global/")
+def api_global():
+    if request.headers.get("Request-Source") != "JS-AJAX":
+        return redirect(url_for("index"))
+    
+    now = datetime.today()
+
+    date_of_today = now.strftime('%d-%m-%Y')
+
+    query = request.args.get("query")
+    if query == "today":
+        return date_of_today
+
+
 @app.route('/api/index/dates')
 def api_dates():
     if request.headers.get("Request-Source") != "JS-AJAX":
