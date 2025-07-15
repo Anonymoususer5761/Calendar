4f131a9a32af8a217fef0f7f03aaa19d1b954184
anony <anonymoususer5761@gmail.com>
2025-07-15 12:22:38 +0530

    Shited the timeline vertically | Part II

--- helpers/line.py
+++ helpers/line.py
@@ -1,4 +1,4 @@
 with open("helpers/file.txt", "w") as f:
     for i in range(25):
-        x_axis = (100 * i) + 100
-        f.write(f'<line class="hours" x1="{x_axis}" y1="20" x2="{x_axis}" y2="510"/>\n')
+        y_axis = (200 * i) + 100
+        f.write(f'<line class="hours" x1="50" y1={y_axis} x2="1475" y2={y_axis}></line>\n')
