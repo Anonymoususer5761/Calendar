c0487e2cee837e2625f243dd08e80e6db00f664a
anony <anonymoususer5761@gmail.com>
2025-07-20 10:38:17 +0530

    Bug Fixes - event SVGs.

--- app/pytemplates.py
+++ app/pytemplates.py
@@ -2,23 +2,36 @@ from app.calendar_db import get_events
 
 from math import trunc
 
-def events_svg(date_id: int|str, user_id: int|str):
+SECONDS_IN_DAY = 86400
+SCALE = 36 # 36 = 86400 (number of seconds in a day) / 2400 (the difference in pixels between 00:00 and 24:00 is exactly 2400px, i.e., 2600 - 200).
+OFFSET = 200 # The timeline starts at y=200px.
+
+def events_svg(date_id: int | str, user_id: int | str) -> str:
+    date_id = int(date_id)
     events = get_events(date_id, user_id)
-    html = ""
-    for event in events:
-        start = event["timings_start"]
-        end = event["timings_end"]
-        seconds_in_day = 86400
-        seconds_in_day_by_scale_of_timeline = 36
-        offset = 200
-        y1 = 100
-        possible_y1 = start % seconds_in_day / seconds_in_day_by_scale_of_timeline + offset
-        y2 = 2599
-        if trunc(start / seconds_in_day) + 1 == int(date_id):
-            y1 = possible_y1
-        elif possible_y1 > 2400:
-            y1 = possible_y1 - 2400
-        if trunc(end / seconds_in_day) + 1 == int(date_id):
-            y1 = end % seconds_in_day / seconds_in_day_by_scale_of_timeline + offset
-        html += f'<polyline id="event-{event["id"]}" class="custom-lines" name="{event["name"]}" points="975,{y1} 75,{y1} 75,{y2} 975,{y2}" fill={event["color"]} stroke={event["color"]} opacity="0.25"></polyline>'
-    return html
+    html = []
+    x1 = 175
+    x2 = 75
+    if events:
+        for event in events:
+            start = event["timings_start"]
+            end = event["timings_end"]
+            y1 = 200
+            possible_y1 = start % SECONDS_IN_DAY / SCALE + OFFSET
+            y2 = 2599
+            if trunc(start / SECONDS_IN_DAY) + 1 == date_id: # Checks if event starts today.
+                y1 = possible_y1
+            elif possible_y1 == 200:
+                y1 = possible_y1 - 100
+            elif possible_y1 > 2400:
+                y1 = possible_y1 - 2400
+            elif possible_y1 > 200:
+                y1 = 100
+            if trunc(end / SECONDS_IN_DAY) + 1 == date_id: # Checks if event starts today.
+                y2 = end % SECONDS_IN_DAY / SCALE + OFFSET
+            html.append(f'<polyline id="event-{event["id"]}" class="custom-lines" name="{event["name"]}" points="{x1},{y1} {x2},{y1} {x2},{y2} {x1},{y2}" fill={event["color"]} stroke={event["color"]} opacity="0.25" stroke-linecap="round"></polyline>')
+            x1 += 100
+            x2 += 100
+        return ''.join(html)
+    else:
+        return "No Events Today"
