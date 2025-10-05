e6f9de6922bb25d147e21ae28dd1021fdde89ffa
anony <anonymoususer5761@gmail.com>
2025-10-05 08:44:09 +0530

    Dealt with the blast radius of previous two commits.

--- app/user.py
+++ app/user.py
@@ -1,9 +1,9 @@
 from app.database_manager import get_db
 from app import login_manager
+from app.forms import LoginForm
 
 from flask_login import UserMixin, login_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import sqlite3
 
 default_settings = {
     "color-palette": 1,
@@ -32,20 +32,12 @@ class User(UserMixin):
 
         if user:
             user_settings = get_user_settings(user["id"])
-            if user_settings:
-                return cls(
-                    user["id"],
-                    user["username"],
-                    user["email"],
-                    user["password_hash"],
-                    user_settings
-                )
             return cls(
                 user["id"],
                 user["username"],
                 user["email"],
                 user["password_hash"],
-                cls.default_settings
+                user_settings
             )
             
         return None
@@ -53,30 +45,23 @@ class User(UserMixin):
     def set_user_settings(self, setting_id, option_id):
         db = get_db()
 
-        exists_query = """SELECT * FROM settings WHERE user_id = ? AND setting_id = ?"""
-
-        sql_stmt = ""
-        if (db.execute(exists_query, (self.id, setting_id))).fetchone():
-            sql_stmt = """UPDATE settings
-SET option_id = ?
-WHERE setting_id = ?
-AND user_id = ?"""
-        else:
-            sql_stmt = """INSERT INTO settings (option_id, setting_id, user_id) 
-VALUES (?, ?, ?)"""
-
         try:
-            db.execute(sql_stmt, (option_id, setting_id, self.id,))
-        except Exception:
-            db.rollback()
+            db.execute("""
+                UPDATE settings
+                SET option_id = ?
+                WHERE setting_id = ?
+                AND user_id = ?
+                """, (
+                    option_id,
+                    setting_id,
+                    self.id,
+                    )
+                )
+            db.commit()
+        finally:
             db.close()
-            return False, 403
 
-        db.commit()
-        db.close()
-        
-        return True, 200
-    
+        return True
 
     def __repr__(self):
         return f"username: {self.username}"
@@ -85,29 +70,33 @@ VALUES (?, ?, ?)"""
 def sign_in_user(form):
     db = get_db()
 
-    user = db.execute(
-        "SELECT * FROM users WHERE username = ?", (
-            form.username.data,
-        )
-    ).fetchone()
-
-    if user:
-        if check_password_hash(user["password_hash"] ,form.password.data):
-            user = User(
-                id=user["id"],
-                username=user["username"],
-                email=user["email"],
-                password_hash=user["password_hash"],
+    try:
+        user = db.execute(
+            "SELECT * FROM users WHERE username = ?", (
+                form.username.data,
             )
+        ).fetchone()
 
-            if login_user(user):
-                return True, f"You have been successfully logged in as {user.username}."
+        if user:
+            if check_password_hash(user["password_hash"] ,form.password.data):
+                user = User(
+                    id=user["id"],
+                    username=user["username"],
+                    email=user["email"],
+                    password_hash=user["password_hash"],
+                    settings=get_user_settings(user["id"])
+                )
 
-            return False, "Sorry! Something went wrong."
-        
-        return False, "Incorrect password."
+                if login_user(user):
+                    return True, f"You have been successfully logged in as {user.username}."
 
-    return  False, "Username not found."
+                return False, "Sorry! Something went wrong."
+            
+            return False, "Incorrect password."
+
+        return  False, "Username not found."
+    finally:
+        db.close()
 
 
 def register_user(form):
@@ -154,18 +143,24 @@ def register_user(form):
 def get_user_settings(id):
     db = get_db()
 
-    sql_stmt = """SELECT setting, option
-FROM settings
-JOIN users ON settings.user_id = users.id
-JOIN settings_options ON settings.option_id = settings_options.id AND settings.setting_id = settings_options.setting_id
-JOIN settings_name ON settings.setting_id = settings_name.id
-WHERE user_id = ?"""
+    try:
+        user_settings = db.execute("""
+            SELECT setting, option
+            FROM settings
+            JOIN users ON settings.user_id = users.id
+            JOIN settings_options ON settings.option_id = settings_options.id AND settings.setting_id = settings_options.setting_id
+            JOIN settings_name ON settings.setting_id = settings_name.id
+            WHERE user_id = ?
+        """, (
+            id,
+        )
+        ).fetchall()
 
-    user_settings = db.execute(sql_stmt, (id,)).fetchall()
+        dict_user_settings = {setting["setting"]:setting["option"] for setting in user_settings}
 
-    dict_user_settings = {setting["setting"]:setting["option"] for setting in user_settings}
-
-    return dict_user_settings
+        return dict_user_settings
+    finally:
+        db.close()
 
 
 @login_manager.user_loader
