eea876173508902101181f76e1b33ede7b4c95f6
anony <anonymoususer5761@gmail.com>
2025-10-12 09:37:48 +0530

    Improved the navbar.

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,34 +1,3 @@
-async function getUserSettings() {
-    const response = await fetch(`/api/settings/get-settings?`, {
-        headers: {
-            'Request-Source': 'JS-AJAX'
-        }
-    });
-    const option = await response.json();
-    return option;
-}
-
-// AI Usage Disclaimer: Needed some help from the AI to make the async part of this work.
-document.addEventListener('DOMContentLoaded', async () => {
-    if (!(localStorage.getItem('dark-mode'))) {
-        if (getUserSettings()['color palette'] === 'dark mode') {
-            localStorage.setItem('dark-mode', '2');
-        } else {
-            localStorage.setItem('dark-mode', '1');
-        }
-    }
-    var darkModeStatus = localStorage.getItem('dark-mode');
-    if (darkModeStatus === '2') {
-        let colorPalette = document.getElementById('color-palette');
-        if (colorPalette) {
-            colorPalette.setAttribute('checked', 'checked');
-        }
-        document.querySelector('link').setAttribute('href', '../static/dark-style.css'); 
-    } else {
-        document.querySelector('link').setAttribute('href', '../static/style.css'); 
-    }
-});
-
 async function isAuthenticated() {
     let response = await fetch('api/global?auth=return', {
         headers: {
