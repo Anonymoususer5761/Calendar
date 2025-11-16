0c4cc5ab82ecc423faac73a8ebc72dc9a68248de
anony <anonymoususer5761@gmail.com>
2025-11-16 12:52:17 +0530

    Updated  js for the calendar table.

--- app/static/index.js
+++ app/static/index.js
@@ -27,6 +27,8 @@ document.addEventListener('shown.bs.dropdown', (event) => {
     }
 })
 
+const daysInAWeek = 7;
+
 let CurrentDate = new Date();
 let currentDay = CurrentDate.getDate();
 let currentMonth = CurrentDate.getMonth() + 1;
@@ -44,14 +46,16 @@ function convertToCSSFormat(category) {
     return category;
 }
 
-function createCalendarElement(elementToCreate, extraClasses) {
+function createCalendarElement(elementToCreate, id, extraClasses) {
     switch(elementToCreate) {
         case 'tr':
             const row = document.createElement('tr');
+            row.id = id
             row.classList.add('calendar-tr', ...(extraClasses ?? []));
             return row;
         case 'td':
             const cell = document.createElement('td');
+            cell.id = id
             cell.classList.add('calendar-td', ...(extraClasses ?? []));
             return cell;
     }
@@ -66,74 +70,72 @@ async function getCalendar() {
         }
     });
     let dates = await datesResponse.json();
+
     const tableBody = document.getElementById('calendar-tbody');
 
+    let dayOfWeek = 1;
+    let preceedingDate = '00';
+    let rows = []
+    let rowIndex = 0;
     for (let date of dates) {
         // If current element is the first.
         if (date['date'] === '01') {
-            tableBody.append(createCalendarElement('tr'));
+            rows.push(createCalendarElement('tr'));
         }
-    }
 
-    // let tableRowStart = '<tr class="calendar-tr">';
-    // let tableRowEnd = '</tr>';
-    // let emptyCells = '<td class="calendar-td"></td>';
-    // let cellEnd = `</td>`;
-    // let cell_class = 'class="calendar-td calendar-td-data"';
-    let html = '';
-    let dayOfWeek = 1;
-    let preceedingDate = "00";
-    for (let date of dates) {
-        tableBody.append(tableRow)
-        if (date['date'] === "01") {
-            html += tableRowStart;
-        }
-        if (preceedingDate == date['date']) {
-            additionalClasses.set(date['id'], convertToCSSFormat(date['category']))
-        //     // document.getElementById(date['date']).classList.add(date["category"])
+        // To differentiate different types of holidays.
+        if (preceedingDate === date['date']) {
+            additionalClasses.set(date['id'], convertToCSSFormat(date['category']));
             continue;
-        } else {
-            preceedingDate = date['date'];
         }
+        preceedingDate = date['date'];
+
         // Prints empty cells if the starting day of the month and the calendar format do not match.  
         while (dayOfWeek != date['day_id']) {
-            html += emptyCells;
-            if (dayOfWeek + 1 < 8) {
-                dayOfWeek = dayOfWeek + 1;
+            let emptyCell = createCalendarElement('td');
+            rows[rowIndex].append(emptyCell);
+            if (dayOfWeek <= daysInAWeek) {
+                dayOfWeek++;
             } else {
-                html += tableRowEnd;
-                html += tableRowStart;
-                dayOfWeek = 1;
+                rows.push(createCalendarElement('tr'));
+                rowIndex++;
+                dayOfWeek = 1
             }
         }
+
         // Prints the date otherwise.
         if (dayOfWeek === date['day_id']) {
+            let extraClasses = [];
             if (date['date'] == currentDay && month.value == currentMonth && year.value == currentYear) {
-                html += `<td id="${date['id']}"" class="today calendar-td calendar-td-data ${convertToCSSFormat(date['category'])}">`;
-                html += date['date'];
-                html += cellEnd;
+                extraClasses = ['calendar-td-data', convertToCSSFormat(date['category'], 'today')];
             } else {
-                html += `<td id="${date['id']}" class="calendar-td calendar-td-data ${convertToCSSFormat(date["category"])}">`;
-                html += date['date'];
-                html += cellEnd;
+                extraClasses = ['calendar-td-data', convertToCSSFormat(date['category'])];
             }
-            if (date['date'] === dates[dates.length - 1]['date']) {
-                let range = 7 - dayOfWeek;
+            let id = toString(date['id']);
+            let cell = createCalendarElement('td', id, extraClasses);
+            cell.textContent = date['date'];
+            rows[rowIndex].append(cell);
+            if (date['date'] === dates[dates.length -1]['date']) {
+                let range = daysInAWeek - dayOfWeek;
                 for (let i = 0; i < range; i++) {
-                    html += emptyCells;
+                    let emptyCell = createCalendarElement('td');
+                    rows[rowIndex].append(emptyCell); 
                 }
             }
         }
-        if (dayOfWeek + 1 < 8) {
-            dayOfWeek = dayOfWeek + 1;
+
+        if (dayOfWeek <= daysInAWeek) {
+            dayOfWeek++;
         } else {
-            html += tableRowEnd;
-            html += tableRowStart;
+            rows.push(createCalendarElement('tr'));
+            rowIndex++;
             dayOfWeek = 1;
         }
     }
 
-    document.getElementById('calendar-tbody').innerHTML = html;
+    for (let row of rows) {
+        tableBody.append(row);
+    }
 
     for (let additionalClass of additionalClasses.entries()) {
         document.getElementById(additionalClass[0]).classList.add(convertToCSSFormat(additionalClass[1]))
@@ -141,70 +143,70 @@ async function getCalendar() {
 }
 
 getCalendar().then( () => {
-    finishCalendar();
+    // finishCalendar();
 });
 month.addEventListener('change', () => {
     getCalendar().then( () => {
-        finishCalendar();
+        // finishCalendar();
     });
 });
 year.addEventListener('change', () => {
     getCalendar().then( () => {
-        finishCalendar();
+        // finishCalendar();
     });
 });
 
-async function getHolidays(day) {
-    let response = await fetch(`/api/index/holidays?id=${day.getAttribute('id')}`, {
-        headers: {
-            'Request-Source': 'JS-AJAX',
-        }
-    });
-    let holidays = await response.json();
-    let html = '';
-    if (holidays) {
-        for (let holiday of holidays) {
-            html += `<h1 class="${convertToCSSFormat(holiday["category"])}">${holiday["holiday"]} ${holiday["category"]}</h1>`;
-        }
-    } else {
-        html = '';
-    }
-    if (day.classList.contains('today')) {
-        html += '<h1 class="today">Today</h1>';
-    }
-    return html;
-}
+// async function getHolidays(day) {
+//     let response = await fetch(`/api/index/holidays?id=${day.getAttribute('id')}`, {
+//         headers: {
+//             'Request-Source': 'JS-AJAX',
+//         }
+//     });
+//     let holidays = await response.json();
+//     let html = '';
+//     if (holidays) {
+//         for (let holiday of holidays) {
+//             html += `<h1 class="${convertToCSSFormat(holiday["category"])}">${holiday["holiday"]} ${holiday["category"]}</h1>`;
+//         }
+//     } else {
+//         html = '';
+//     }
+//     if (day.classList.contains('today')) {
+//         html += '<h1 class="today">Today</h1>';
+//     }
+//     return html;
+// }
 
-async function finishCalendar() {
-    let tooltipExists = false;
-    async function hideTooltip(event) {
-        if (tooltipExists) {
-            event.target.removeChild(event.target._divRef);
-            tooltipExists = false;
-        }
-    }
-    async function showTooltip(event, html) {
-        await hideTooltip();
-        let div = document.createElement('div');
-        div.className = 'calendar-td-data-tooltip card border-info mb-3';
-        event.target.appendChild(div);
-        event.target._divRef = div;
-        tooltipExists = true;
-        div.innerHTML = html;
+// async function finishCalendar() {
+//     let tooltipExists = false;
+//     async function hideTooltip(event) {
+//         if (tooltipExists) {
+//             event.target.removeChild(event.target._divRef);
+//             tooltipExists = false;
+//         }
+//     }
+//     async function showTooltip(event, html) {
+//         await hideTooltip();
+//         let div = document.createElement('div');
+//         div.className = 'calendar-td-data-tooltip card border-info mb-3';
+//         event.target.appendChild(div);
+//         event.target._divRef = div;
+//         tooltipExists = true;
+//         div.innerHTML = html;
 
-    }
-    let dayCells = document.getElementsByClassName('calendar-td-data');
-    for (let cell of dayCells) {
-        cell.addEventListener('click', (event) => {
-            document.location.href = `/dates?id=${event.target.getAttribute('id')}`;
-        });
-        getHolidays(cell).then((html) => {
-            if (html) {
-                cell.addEventListener('mouseover', (event) => {
-                    showTooltip(event, html);
-                });
-            }
-        });
-        cell.addEventListener('mouseout', hideTooltip);
-    }
-}
+//     }
+//     let dayCells = document.getElementsByClassName('calendar-td-data');
+//     for (let cell of dayCells) {
+//         cell.addEventListener('click', (event) => {
+//             document.location.href = `/dates?id=${event.target.getAttribute('id')}`;
+//         });
+//         getHolidays(cell).then((html) => {
+//             if (html) {
+//                 cell.addEventListener('mouseover', (event) => {
+//                     showTooltip(event, html);
+//                 });
+//             }
+//         });
+//         cell.addEventListener('mouseout', hideTooltip);
+//     }
+// }
