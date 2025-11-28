57c94a4c4d80ae6b867619ebdd7e62845372784e
anony <anonymoususer5761@gmail.com>
2025-11-28 16:03:31 +0530

    Bug fixes: event svgs would not display the correct duration

--- app/pytemplates.py
+++ app/pytemplates.py
@@ -3,7 +3,7 @@ from app.calendar_db import get_events
 from datetime import timedelta
 
 SECONDS_IN_DAY = 86400
-PIXELS_IN_TIMELINE = 2500
+PIXELS_IN_TIMELINE = 2400
 SCALE = SECONDS_IN_DAY / PIXELS_IN_TIMELINE
 
 SECONDS_IN_HOUR = 3600
@@ -26,16 +26,19 @@ def get_event_svg(date_id: int | str, user_id: int | str) -> str:
             event_start = timedelta(seconds=event["start_time"])
             event_end = timedelta(seconds=event["end_time"])
             y1 = 0
+            y2 = 2499
             if event_start.days == selected_date.days:
                 y1 = event_start.seconds / SCALE + DAY_START_IN_PIXELS
             elif event_start - selected_date >= timedelta(seconds=SECONDS_IN_HOUR):
                 y1 = event_start.seconds / SCALE + DAY_START_IN_PIXELS - ONE_DAY_IN_PIXELS
             if event_end.days == selected_date.days:
-                y2 = event_end.seconds / SCALE + DAY_START_IN_PIXELS
+                y2 = event_end.seconds / SCALE + DAY_START_IN_PIXELS - y1
             elif event_end - selected_date <= timedelta(seconds=SECONDS_IN_HOUR):
-                y2 = event_end.seconds / SCALE + DAY_START_IN_PIXELS - ONE_DAY_IN_PIXELS
+                y2 = event_end.seconds / SCALE + DAY_START_IN_PIXELS - ONE_DAY_IN_PIXELS - y1
+            if y2 == 0:
+                y2 = 1
             html.append(
-                f'<rect value={event["id"]} class="event-rects" x="{x1_percent}%" y="{y1}" width="{x2_percent}%" height="{y2}" fill="{event["color"]}"></rect>'
+                f'<rect value={event["id"]} class="event-rects" x="{x1_percent}%" y="{y1}" width="{x2_percent}%" height="{y2}" fill="{event["color"]}" stroke="{event['color']}"></rect>'
             )
             x1_percent += width_increment
         return ''.join(html)
