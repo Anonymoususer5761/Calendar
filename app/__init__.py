7efad7dcb46b1e22fc2c509ac7d5d769bb17873f
anony <anonymoususer5761@gmail.com>
2025-12-02 09:23:58 +0530

    Added digital-7 fonts.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
